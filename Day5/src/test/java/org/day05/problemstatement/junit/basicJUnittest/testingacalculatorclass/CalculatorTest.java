600a35d68a21f30e97b8b2fb3e1fcae620d32157
Bishtrishika01 <bishtrishika66@gamil.com>
2025-02-15 23:29:51 +0530

    [Rishika] Add. Calculator

---
+++ Day5/src/test/java/org/day05/problemstatement/junit/basicJUnittest/testingacalculatorclass/CalculatorTest.java
@@ -0,0 +1,29 @@
+package org.day05.problemstatement.junit.basicJUnittest.testingacalculatorclass;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void calculatorAddTest(){
+        Calculator calculator=new Calculator();
+        int actual=calculator.add(2,2);
+        assertEquals(4,actual);
+    }@Test
+    void calculatorSubTest(){
+        Calculator calculator=new Calculator();
+        int actual=calculator.subtract(2,2);
+        assertEquals(0,actual);
+    }@Test
+    void calculatorMultiTest(){
+        Calculator calculator=new Calculator();
+        int actual=calculator.multiply(2,2);
+        assertEquals(4,actual);
+    }@Test
+    void calculatorDivideTest(){
+        Calculator calculator=new Calculator();
+        int actual=calculator.divide(4,2);
+        assertEquals(2,actual);
+    }
+}
